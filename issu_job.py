382874ba144b23d0d0ff64af35f1b1aaeb8e71d2
DhavalSharma19 <79095659+DhavalSharma19@users.noreply.github.com>
2023-06-30 09:53:30 +0530

    Update issu_job.py

--- issu_job.py
+++ issu_job.py
@@ -1,1 +1,21 @@
+import re
+import os
+import sys
+import argparse
+import time
 
+from pyats import aetest
+from pyats.easypy import run
+from pyats.datastructures.logic import Or
+from pyats import topology
+
+from pprint import pprint
+from time import sleep 
+
+def main():
+      ''' Main function '''
+
+      # Find the location of the script in relation to the job file, used for autoeasy execution
+      testscript = '/ws/dhavshar-bgl/script/dhaval_script/issu_verification_test_script.py'
+    
+      run(testscript = testscript)
